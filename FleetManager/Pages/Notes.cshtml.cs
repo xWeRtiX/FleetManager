3e41ad48187c4664b311ac6615979201b3b35c85
Maerkish <skaldamarek@gmail.com>
2022-03-09 17:56:59 +0100

    Edited _Layout for normal users and added log for creating notes.

--- FleetManager/Pages/Notes.cshtml.cs
+++ FleetManager/Pages/Notes.cshtml.cs
@@ -1,5 +1,6 @@
 using FleetManager.Data;
 using FleetManager.Services.Interfaces;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -14,6 +15,12 @@ namespace FleetManager.Pages
 
         private readonly INoteService _noteService;
 
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        private readonly ILogService _logService;
+
         public class InputModel
         {
             public string Title { get; set; }
@@ -22,9 +29,12 @@ namespace FleetManager.Pages
 
         }
 
-        public NotesModel(INoteService noteService)
+        public NotesModel(INoteService noteService, UserManager<AppUser> userManager, IHttpContextAccessor httpContextAccessor, ILogService logService)
         {
             _noteService = noteService;
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
+            _logService = logService;
         }
 
         public void OnGet(int carId)
@@ -37,6 +47,8 @@ namespace FleetManager.Pages
             if (Input.Title == null || Input.Description == null) return RedirectToPage("/Notes", new { carId });
 
             _noteService.Create(new Note { CarId = carId, Description = Input.Description, Title = Input.Title });
+            var userId = _userManager.GetUserId(_httpContextAccessor.HttpContext.User);
+            _logService.Log(LogType.INFO, $"Zapsal poznámku k vozidlu ID: {carId} s titulem '{Input.Title}'", Convert.ToInt32(userId));
             return RedirectToPage("/Notes", new { carId });
         }
     }
